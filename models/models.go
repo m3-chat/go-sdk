2ace8eb05cf2be6ac13b9d0636aa71a7c75d9769
myferr <contactme.myfer@protonmail.com>
2025-06-18 00:52:24 -0500

    feat: add models

---
+++ models/models.go
@@ -0,0 +1,24 @@
+package models
+
+func GetAvailableModels() []string {
+	return []string{
+		"llama3:8b",
+		"llama2-uncensored",
+		"gemma3",
+		"gemma",
+		"phi3:mini",
+		"mistral",
+		"gemma:2b",
+		"gemma:7b",
+		"qwen:7b",
+		"qwen2.5-coder",
+		"qwen3",
+		"deepseek-coder:6.7b",
+		"deepseek-v2:16b",
+		"dolphin-mistral:7b",
+		"dolphin3",
+		"starcoder2:7b",
+		"magistral",
+		"devstral",
+	}
+}
